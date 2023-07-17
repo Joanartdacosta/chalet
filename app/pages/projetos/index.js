4ab53b6abeaba78fa86b4d05a11294a9a7678ef6
joanartdacosta <joanartdacosta@gmail.com>
2023-07-17 19:50:50 +0200

    complete navbar links - new components

---
+++ app/pages/projetos/index.js
@@ -0,0 +1,11 @@
+// import ProjectsList from "..."
+// import PROJECTS
+
+export default function Projects() {
+  return (
+    <>
+      <h1>PROJETOS</h1>
+      {/* <ProjectsList ProjectsGallery={PROJECTS} /> */}
+    </>
+  );
+}

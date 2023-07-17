fe3de93428b5d130a26b1b8594826bbf73bd2785
joanartdacosta <joanartdacosta@gmail.com>
2023-07-17 19:37:40 +0200

    add navbar in homepage

---
+++ app/components/NavBar.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <>
+      <Link href="/about">Sobre mim</Link>
+      <Link href="">Contactos</Link>
+      <Link href="">Servicos</Link>
+      <Link href="">Projetos</Link>
+    </>
+  );
+}

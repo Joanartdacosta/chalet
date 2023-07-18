f120fd4149131f96816254f40c063163985ba35c
joanartdacosta <joanartdacosta@gmail.com>
2023-07-18 14:29:18 +0200

    add background in homepage

--- app/pages/index.js
+++ app/pages/index.js
@@ -7,36 +7,38 @@ import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
+    <div className={utilStyles.background}>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
 
-      <section className={utilStyles.headingMd}>
-        <p>DESIGN de INTERIORES</p>
-        <p>by</p>
-        <p>Mariana Batista Ramadas</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{" "}
-          <Link href="/about">our Next.js tutorial</Link>.)
-        </p>
-      </section>
+        <section>
+          <p>DESIGN de INTERIORES</p>
+          <p>by</p>
+          <p>Mariana Batista Ramadas</p>
+          <p>
+            (This is a sample website - you’ll be building a site like this in{" "}
+            <Link href="/about">our Next.js tutorial</Link>.)
+          </p>
+        </section>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </Layout>
+    </div>
   );
 }
 

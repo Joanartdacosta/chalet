10f81ea81393a76514a6c306eb4a9744b2967811
joanartdacosta <joanartdacosta@gmail.com>
2023-07-18 14:05:52 +0200

    styling navbar

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,22 +1,25 @@
 import Link from "next/link";
-import utilStyles from "../styles/utils.module.css";
 import styles from "./layout.module.css";
 
 export default function NavBar() {
   return (
     <>
-      <Link className={styles.navBar} href="/about">
-        SOBRE MIM
-      </Link>
-      <Link className={styles.navBar} href="/contactos">
-        CONTACTOS
-      </Link>
-      <Link className={styles.navBar} href="/services">
-        SERVICOS
-      </Link>
-      <Link className={styles.navBar} href="/projetos">
-        PROJETOS
-      </Link>
+      <div className={styles.menuLeft}>
+        <Link className={styles.navBarLink} href="/about">
+          SOBRE MIM
+        </Link>
+        <Link className={styles.navBarLink} href="/contactos">
+          CONTACTOS
+        </Link>
+      </div>
+      <div className={styles.menuRight}>
+        <Link className={styles.navBarLink} href="/services">
+          SERVICOS
+        </Link>
+        <Link className={styles.navBarLink} href="/projetos">
+          PROJETOS
+        </Link>
+      </div>
     </>
   );
 }

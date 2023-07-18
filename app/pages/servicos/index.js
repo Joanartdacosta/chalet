f8083bcdb6f6b4737f26fe666763e31d059be956
joanartdacosta <joanartdacosta@gmail.com>
2023-07-18 12:53:39 +0200

    add layout - contacts, projects and services pages

---
+++ app/pages/servicos/index.js
@@ -0,0 +1,14 @@
+// import SERVICES from "../../components/services/";
+// import ServicesList from "";
+import Layout from "../../components/layout";
+
+export default function Services() {
+  return (
+    <>
+      <Layout>
+        <h1>SERVICOS</h1>
+        {/* <ServicesList services={SERVICES} /> */}
+      </Layout>
+    </>
+  );
+}

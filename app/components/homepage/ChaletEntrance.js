4b415503184abb94993f4d30c437044bcd9d61c0
joanartdacosta <joanartdacosta@gmail.com>
2023-07-19 17:17:21 +0200

    add title and hr style

--- app/components/homepage/ChaletEntrance.js
+++ app/components/homepage/ChaletEntrance.js
@@ -3,6 +3,8 @@ import styles from "./homeservices.module.css";
 export default function ChaletBackground() {
   return (
     <div>
+      <h1 className={styles.chaletTitle}>CHALET</h1>
+      <hr className={styles.hr} />
       <h1>DESIGN de INTERIORES</h1>
       <p>by</p>
       <h1 className={styles.designerName}>Mariana Batista Ramadas</h1>

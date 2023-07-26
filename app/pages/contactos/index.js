12d282d40cf77870de797584ba7f4656d554e03d
joanartdacosta <joanartdacosta@gmail.com>
2023-07-26 19:08:13 +0200

    contacts page module css

--- app/pages/contactos/index.js
+++ app/pages/contactos/index.js
@@ -1,14 +1,20 @@
 // import Form from "../../components/contacts/Form";
 import Layout from "../../components/layout";
+import layoutStyles from "../../components/layout.module.css";
+import contactsStyles from "../../components/contacts.module.css";
 
 export default function Contacts() {
   return (
-    <>
+    <section className={layoutStyles.informationSection}>
       <Layout>
-        <h1>CONTACTOS</h1>
-
+        <h1 className={layoutStyles.subtitles}>CONTACTOS</h1>
+        <img
+          className={contactsStyles.detailImg}
+          src="https://chalet-webapp.s3.amazonaws.com/designer-photos/2024.JPG"
+          alt="details"
+        ></img>
         {/* <Form/> */}
       </Layout>
-    </>
+    </section>
   );
 }

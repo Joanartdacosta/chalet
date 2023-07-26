e9e9c1cb2307675c9140cddf710891d071bf2416
joanartdacosta <joanartdacosta@gmail.com>
2023-07-26 19:06:54 +0200

    changes in homepage

--- app/pages/index.js
+++ app/pages/index.js
@@ -4,22 +4,23 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import ChaletEntrance from "../components/homepage/ChaletEntrance";
 import Mission from "../components/homepage/Mission";
-// import HomeSections from "../components/homepage/HomeSections";
-import styles from "../components/homepage/homeservices.module.css";
+import HomeSections from "../components/homepage/HomeSections";
+import homeStyles from "../components/homepage/home.module.css";
+import layoutStyles from "../components/layout.module.css";
 
 export default function Home() {
   return (
     <div>
       <Head />
-      <section className={styles.background}>
+      <section className={homeStyles.background}>
         <Layout home>
           <ChaletEntrance />
         </Layout>
       </section>
-      <section>
+      <section className={layoutStyles.informationSection}>
         <Mission />
+        <HomeSections />
       </section>
-      {/* <HomeSections /> */}
     </div>
   );
 }

40f3e8cc3bec9951dc126392a9f6f1ff73a1f934
joanartdacosta <joanartdacosta@gmail.com>
2023-07-27 18:33:54 +0200

    general corrections

--- app/pages/projetos/index.js
+++ app/pages/projetos/index.js
@@ -6,7 +6,6 @@ export default function Projects() {
     <section className={layoutStyles.informationSection}>
       <Layout>
         <h1 className={layoutStyles.subtitles}>PROJECTOS</h1>
-        <h1>PROJETOS</h1>
       </Layout>
     </section>
   );

fb050d8d6580d728e81b3066bb750074cf88e90b
joanartdacosta <joanartdacosta@gmail.com>
2023-07-28 21:39:39 +0200

    sections and create card

---
+++ app/components/homepage/SECTIONS.js
@@ -0,0 +1,25 @@
+const SECTIONS = [
+  {
+    id: 2,
+    title: "Sobre",
+    imgURL:
+      "https://chalet-webapp.s3.amazonaws.com/designer-photos/IMG_1564.JPG",
+    href: "/sobre",
+  },
+  {
+    id: 2,
+    title: "Servicos",
+    imgURL:
+      "https://chalet-webapp.s3.amazonaws.com/moodboards/Moodboard+cozinha1.png",
+    href: "/servicos",
+  },
+  {
+    id: 3,
+    title: "Projetos",
+    imgURL:
+      "https://chalet-webapp.s3.amazonaws.com/Servicos/remodelacao/moradia-mezzanine/sala/10.png",
+    href: "/projetos",
+  },
+];
+
+export default SECTIONS;

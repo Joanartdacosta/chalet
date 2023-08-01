3273fee4796cf3d56abb90b34353ec551a79eb80
joanartdacosta <joanartdacosta@gmail.com>
2023-08-01 20:31:29 +0200

    delete files and layout components

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,33 +1,22 @@
 import Head from "next/head";
-import Layout from "../components/layout";
-import { getSortedPostsData } from "../lib/posts";
 import ChaletEntrance from "../components/homepage/ChaletEntrance";
 import Mission from "../components/homepage/Mission";
 
 import homeStyles from "../components/homepage/home.module.css";
 import layoutStyles from "../components/layout.module.css";
+import HomeSections from "../components/homepage/HomeSections";
 
 export default function Home() {
   return (
     <div>
       <Head />
       <section className={homeStyles.background}>
-        <Layout home>
-          <ChaletEntrance />
-        </Layout>
+        <ChaletEntrance />
       </section>
       <section className={layoutStyles.informationSection}>
         <Mission />
+        <HomeSections />
       </section>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
 }
--- app/pages/projetos/index.js
+++ app/pages/projetos/index.js
@@ -1,12 +1,9 @@
-import Layout from "../../components/layout";
 import layoutStyles from "../../components/layout.module.css";
 
 export default function Projects() {
   return (
     <section className={layoutStyles.informationSection}>
-      <Layout>
-        <h1 className={layoutStyles.subtitles}>PROJECTOS</h1>
-      </Layout>
+      <h1 className={layoutStyles.subtitles}>PROJECTOS</h1>
     </section>
   );
 }

5055c949ab6b6cc04360a62f9d7c8f7a064da558
joanartdacosta <joanartdacosta@gmail.com>
2023-08-10 13:08:52 +0200

    create a different service list design

--- app/components/services/CardService.js
+++ app/components/services/CardService.js
@@ -1,14 +1,31 @@
 import serviceStyles from "./service.module.css";
+import layoutStyles from "../layout.module.css";
 
 export default function CardService(props) {
   return (
-    <div
-      className={serviceStyles.serviceSection}
-      style={{
-        backgroundImage: props.imgURL,
-      }}
-    >
-      {props.title}
+    <div>
+      <div className={serviceStyles.serviceSection}>
+        <div className={serviceStyles.serviceText}>
+          <h1> {props.title}</h1>
+          <p className={serviceStyles.serviceDescription}>
+            {props.description}
+          </p>
+
+          <li className={serviceStyles.serviceBullets}>{props.detail1}</li>
+          <li className={serviceStyles.serviceBullets}>{props.detail2}</li>
+          <li className={serviceStyles.serviceBullets}>{props.detail3}</li>
+          <li className={serviceStyles.serviceBullets}>{props.detail4}</li>
+        </div>
+
+        <div
+          className={serviceStyles.serviceImage}
+          style={{
+            backgroundImage: props.imgURL,
+          }}
+        ></div>
+        <div> </div>
+      </div>
+      <button className={layoutStyles.button}>Saber mais</button>
     </div>
   );
 }
--- app/components/services/createCardService.js
+++ app/components/services/createCardService.js
@@ -6,6 +6,11 @@ export default function createCardService(SERVICES) {
       id={SERVICES.id}
       imgURL={SERVICES.imgURL}
       title={SERVICES.title}
+      description={SERVICES.description}
+      detail1={SERVICES.detail1}
+      detail2={SERVICES.detail2}
+      detail3={SERVICES.detail3}
+      detail4={SERVICES.detail4}
     />
   );
 }

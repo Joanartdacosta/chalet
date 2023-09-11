d7c12b2c025078cddb3e018faaacb3a1a2eb0fae
joanartdacosta <joanartdacosta@gmail.com>
2023-09-11 23:53:46 +0200

    correct project index

--- app/pages/projetos/index.js
+++ app/pages/projetos/index.js
@@ -1,5 +1,5 @@
 import layoutStyles from "../../components/layout/layout.module.css";
-import ProjectMenu from "../../components/projects/project_menu/project_menu/ProjectMenu";
+import ProjectMenu from "../../components/projects/project_menu/ProjectMenu";
 import ProjectsGallery from "../../components/projects/gallery/ProjectsGallery";
 import Link from "next/link";
 import projectStyles from "../../components/projects/projects.module.css";
@@ -9,12 +9,12 @@ export default function Projects() {
     <div className={layoutStyles.informationSection}>
       <h1 className={layoutStyles.subtitles}>PROJECTOS</h1>
       <ProjectMenu />
-
       <div className={projectStyles.conceptBoard}>
         <Link className={projectStyles.projectTitle} href={"#concept-board"}>
           <p>Concept Boards</p>
         </Link>
       </div>
+
       <ProjectsGallery />
     </div>
   );

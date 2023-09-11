6fabc6226b2faf777c3d9d85f282dc45ac096fef
joanartdacosta <joanartdacosta@gmail.com>
2023-09-11 22:51:22 +0200

    simple gallery effects react masonry

--- app/components/projects/gallery/ProjectsGallery.js
+++ app/components/projects/gallery/ProjectsGallery.js
@@ -1,80 +1,92 @@
 import React from "react";
-// import COMPLEMENTARY from "../../services/galleries/complementary";
-// import ONLINE from "../../services/galleries/online";
-// import DESIGN from "../../services/galleries/design";
-// import HOME_STANGING from "../../services/galleries/homeStaging";
-// import DECOR from "../../services/galleries/decor";
-// import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import COMPLEMENTARY from "../../services/galleries/complementary";
+import ONLINE from "../../services/galleries/online";
+import DESIGN from "../../services/galleries/design";
+import HOME_STANGING from "../../services/galleries/homeStaging";
+import DECOR from "../../services/galleries/decor";
 import projectStyles from "../projects.module.css";
-import createCardProject from "../project_menu/createCardProject";
-import SERVICES from "../../services/services";
-import { useState } from "react";
-import ALL from "../../services/galleries/all";
-//https://www.altogic.com/blog/passing-data-from-parent-to-child-in-react
+
 export default function ProjectsGallery() {
-  const [category1, setCategory1] = useState("");
-  const [category2, setCategory2] = useState("");
-
-  function filterImages() {
-    const images = ALL.filter((image) => {
-      if (image.category1 === category1 && image.category2 === category2) {
-        return image;
-      }
-    });
-
-    console.log(images);
-  }
-
-  function handleCategory1(event) {
-    setCategory1(event.target.value);
-
-    filterImages();
-  }
-
-  function handleCategory2(event) {
-    setCategory2(event.target.value);
-
-    filterImages();
-  }
-
   return (
     <div>
-      <div>
-        <div className={projectStyles.buttonMenu}>
-          <button
-            value="consultoria"
-            className={projectStyles.buttonPlace}
-            onClick={handleCategory1}
-          >
-            Consultoria
-          </button>
-          <button
-            value="consultoriaOnline"
-            className={projectStyles.buttonPlace}
-            onClick={handleCategory1}
-          >
-            Consultoria Online
-          </button>
-        </div>
+      <div id={"complementar"}>
+        <h1 className={projectStyles.galleryTitle}>Consultoria Complementar</h1>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+          <Masonry>
+            {COMPLEMENTARY.map((photo) => (
+              <img
+                className={projectStyles.galleryMasonry}
+                src={photo.imgURL}
+                alt="complementary-photo"
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
       </div>
 
-      <div>
-        <div className={projectStyles.buttonMenu}>
-          <button
-            value="cozinha"
-            className={projectStyles.buttonPlace}
-            onClick={handleCategory2}
-          >
-            Cozinha
-          </button>
-          <button
-            value="sala"
-            className={projectStyles.buttonPlace}
-            onClick={handleCategory2}
-          >
-            Sala
-          </button>
-        </div>
+      <div id={"online"}>
+        <h1 className={projectStyles.galleryTitle}>Consultoria Online</h1>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+          <Masonry>
+            {ONLINE.map((photo) => (
+              <img
+                className={projectStyles.galleryMasonry}
+                src={photo.imgURL}
+                alt="home-staging-photo"
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      </div>
+
+      <div id={"design"}>
+        <h1 className={projectStyles.galleryTitle}>
+          Projetos de Design de Interiores
+        </h1>
+        <ResponsiveMasonry>
+          <Masonry columnsCount={3}>
+            {DESIGN.map((photo) => (
+              <img
+                className={projectStyles.galleryMasonry}
+                src={photo.imgURL}
+                alt="design-photo"
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      </div>
+
+      <div id={"decor"}>
+        <h1 className={projectStyles.galleryTitle}>
+          Projetos de Decoracao de Interiores
+        </h1>
+        <ResponsiveMasonry>
+          <Masonry columnsCount={3}>
+            {DECOR.map((photo) => (
+              <img
+                className={projectStyles.galleryMasonry}
+                src={photo.imgURL}
+                alt="decor-photo"
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      </div>
+
+      <div id={"staging"}>
+        <h1 className={projectStyles.galleryTitle}>Home Staging</h1>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+          <Masonry>
+            {HOME_STANGING.map((photo) => (
+              <img
+                className={projectStyles.galleryMasonry}
+                src={photo.imgURL}
+                alt="home-staging-photo"
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
       </div>
     </div>
   );
--- app/pages/projetos/index.js
+++ app/pages/projetos/index.js
@@ -1,11 +1,13 @@
 import layoutStyles from "../../components/layout/layout.module.css";
-import ProjectMenu from "../../components/projects/ProjectMenu";
+import ProjectMenu from "../../components/projects/project_menu/ProjectMenu";
+import ProjectsGallery from "../../components/projects/gallery/ProjectsGallery";
 
 export default function Projects() {
   return (
     <div className={layoutStyles.informationSection}>
       <h1 className={layoutStyles.subtitles}>PROJECTOS</h1>
       <ProjectMenu />
+      <ProjectsGallery />
     </div>
   );
 }

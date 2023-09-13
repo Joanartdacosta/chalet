826e17ea0e50881697369e0618f0521a25466de4
joanartdacosta <joanartdacosta@gmail.com>
2023-09-14 00:47:15 +0200

    improve style of services page

--- app/components/services/ModalService.js
+++ app/components/services/ModalService.js
@@ -2,8 +2,8 @@ import serviceStyle from "./service.module.css";
 
 export default function ModalService(props) {
   return (
-    <div>
-      <p>Mais informacoes</p>
+    <div className={serviceStyle.modal}>
+      <h3>Mais informacoes</h3>
       <p>{props.step1}</p>
       <p>{props.step2}</p>
       <p>{props.step3}</p>

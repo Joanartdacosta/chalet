1a43dbfa36bd9a0b62bb8feee7dfe3210e5e41b9
joanartdacosta <joanartdacosta@gmail.com>
2023-09-14 00:23:24 +0200

    add next fonts

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,5 +1,26 @@
 import Layout from "../components/layout/layout";
 import "../styles/global.css";
+import { Poppins } from "@next/font/google";
+import { Roboto } from "@next/font/google";
+import { WindSong } from "@next/font/google";
+
+const poppins = Poppins({
+  variable: "--poppins-font",
+  subsets: ["latin"],
+  weight: "200",
+});
+
+const roboto = Roboto({
+  variable: "--roboto-font",
+  subsets: ["latin"],
+  weight: "100",
+});
+
+const windsong = WindSong({
+  variable: "--windsong-font",
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function App({ Component, pageProps }) {
   return (

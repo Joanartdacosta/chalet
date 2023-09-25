446a900991545fec1476b0a8cc721b5fdf9ca953
joanartdacosta <joanartdacosta@gmail.com>
2023-09-25 16:30:59 +0200

    add categoryMenu component

--- app/pages/projetos/index.js
+++ app/pages/projetos/index.js
@@ -1,13 +1,17 @@
 import layoutStyles from "../../components/layout/layout.module.css";
 import ProjectMenu from "../../components/projects/project_menu/ProjectMenu";
 import ProjectsGallery from "../../components/projects/gallery/ProjectsGallery";
+import CategoryMenu from "../../components/projects/categories/CategoryMenu";
 
 export default function Projects() {
   return (
     <div className={layoutStyles.informationSection}>
       <h1 className={layoutStyles.subtitles}>PROJECTOS</h1>
-      <ProjectMenu />
-      <ProjectsGallery />
+      <div>
+        <ProjectMenu />
+        <CategoryMenu />
+        <ProjectsGallery />
+      </div>
     </div>
   );
 }

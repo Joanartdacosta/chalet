94515c124e3535597d4cebadf190d47395469a73
joanartdacosta <joanartdacosta@gmail.com>
2023-09-25 17:42:31 +0200

    fix categories filter

--- app/pages/projetos/index.js
+++ app/pages/projetos/index.js
@@ -2,16 +2,20 @@ import layoutStyles from "../../components/layout/layout.module.css";
 import ProjectMenu from "../../components/projects/project_menu/ProjectMenu";
 import ProjectsGallery from "../../components/projects/gallery/ProjectsGallery";
 import CategoryMenu from "../../components/projects/categories/CategoryMenu";
+import { useState } from "react";
+import ALL from "../../components/projects/galleries/all";
+import GalleryFiltered from "../../components/projects/gallery/GalleryFiltered";
 
 export default function Projects() {
+  const [categories, setCategories] = useState(ALL);
+
   return (
     <div className={layoutStyles.informationSection}>
       <h1 className={layoutStyles.subtitles}>PROJECTOS</h1>
-      <div>
-        <ProjectMenu />
-        <CategoryMenu />
-        <ProjectsGallery />
-      </div>
+      <ProjectMenu />
+      <CategoryMenu setCategories={setCategories} />
+      <GalleryFiltered categories={categories} />
+      <ProjectsGallery />
     </div>
   );
 }

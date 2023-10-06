d160a9a33cb7d879e3d6c457fa0ffc23c90f10ad
joanartdacosta <joanartdacosta@gmail.com>
2023-10-06 17:40:11 +0200

    fix font changing

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,26 +1,5 @@
 import Layout from "../components/layout/layout";
 import "../styles/global.css";
-import { Poppins } from "next/font/google";
-import { Roboto } from "next/font/google";
-import { WindSong } from "next/font/google";
-
-const POPPINS = Poppins({
-  variable: "--poppins-font",
-  subsets: ["latin"],
-  weight: "200",
-});
-
-const ROBOTO = Roboto({
-  variable: "--roboto-font",
-  subsets: ["latin"],
-  weight: "100",
-});
-
-const WINDSONG = WindSong({
-  variable: "--windsong-font",
-  subsets: ["latin"],
-  weight: "400",
-});
 
 export default function App({ Component, pageProps }) {
   return (

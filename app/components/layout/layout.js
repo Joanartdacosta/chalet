56e2d42cbc440160040a3a9f30c8db462273234c
joanartdacosta <joanartdacosta@gmail.com>
2023-10-07 14:18:02 +0200

    config google fonts

--- app/components/layout/layout.js
+++ app/components/layout/layout.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import NavBar from "../navbar/NavBar";
 import Footer from "../footer/Footer";
 import LinkHome from "../link/LinkHome";
-import { GoogleFonts } from "next-google-fonts";
 import NextHead from "next/head";
 
 const NAME = "CHALET";
@@ -11,26 +10,27 @@ export const DESCRIPTION = "Chalet - Atelier de Design de Interiores";
 export default function Layout({ children, home }) {
   return (
     <div>
-      <Head>
-        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />
-        <NextHead>
-          <title id="title">{NAME}</title>
-          <link
-            rel="icon"
-            href="https://chalet-webapp.s3.amazonaws.com/Logotipo.png"
-          />
+      <NextHead>
+        <title id="title">{NAME}</title>
+        <link
+          rel="icon"
+          href="https://chalet-webapp.s3.amazonaws.com/Logotipo.png"
+        />
 
-          <meta name="description" content={DESCRIPTION} />
-          <meta name="name" content={DESCRIPTION} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Poppins:wght@100;200;300&family=Raleway+Dots&family=Raleway:wght@100&family=WindSong&display=swap"
+          rel="stylesheet"
+        />
 
-          <meta name="keywords" content="Atelier, Design, Decoracao" />
-          <meta name="author" content={NAME} />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-        </NextHead>
-      </Head>
+        <meta name="description" content={DESCRIPTION} />
+        <meta name="name" content={DESCRIPTION} />
+
+        <meta name="keywords" content="Atelier, Design, Decoracao" />
+        <meta name="author" content={NAME} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </NextHead>
 
       <main>
         {" "}

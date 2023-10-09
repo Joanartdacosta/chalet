f40d567c15443dac94a4dce0fc7c0fae7ab69c37
joanartdacosta <joanartdacosta@gmail.com>
2023-10-09 18:37:49 +0200

    remove category

--- app/pages/projetos/index.js
+++ app/pages/projetos/index.js
@@ -1,7 +1,6 @@
 import layoutStyles from "../../components/layout/layout.module.css";
 import ProjectMenu from "../../components/projects/project_menu/ProjectMenu";
 import ProjectsGallery from "../../components/projects/gallery/ProjectsGallery";
-import CategoryMenu from "../../components/projects/categories/CategoryMenu";
 import { useState } from "react";
 import ALL from "../../components/projects/galleries/all";
 
@@ -12,7 +11,6 @@ export default function Projects() {
     <div className={layoutStyles.informationSection}>
       <h1 className={layoutStyles.subtitles}>PROJECTOS</h1>
       <ProjectMenu />
-      <CategoryMenu setCategories={setCategories} categories={categories} />
       <ProjectsGallery />
     </div>
   );

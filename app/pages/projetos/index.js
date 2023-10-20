c6c15eebda04d34bd0f38123f5ea5f2280b73f1e
joanartdacosta <joanartdacosta@gmail.com>
2023-10-20 17:55:59 +0200

    responsive webpage - projects with category

--- app/pages/projetos/index.js
+++ app/pages/projetos/index.js
@@ -1,5 +1,6 @@
 import ProjectMenu from "../../components/projects/project_menu/ProjectMenu";
 import ProjectsGallery from "../../components/projects/gallery/ProjectsGallery";
+import CategoryMenu from "../../components/projects/categories/CategoryMenu";
 import { useState } from "react";
 import ALL from "../../components/projects/galleries/all";
 
@@ -9,8 +10,9 @@ export default function Projects() {
   return (
     <div className="padding-top-section">
       <h1 className="font-title text-center">PROJECTOS</h1>
-      <hr className="hr hr-sub margin-auto " />
+      <hr className="hr hr-subtitle margin-auto" />
       <ProjectMenu />
+      <CategoryMenu setCategories={setCategories} categories={categories} />
       <ProjectsGallery />
     </div>
   );

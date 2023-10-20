40faa9c6435d80a4f7af53a323bee1d201bfec89
joanartdacosta <joanartdacosta@gmail.com>
2023-10-20 16:40:51 +0200

    change photos in designer gallery

--- app/components/about/designer.js
+++ app/components/about/designer.js
@@ -9,13 +9,13 @@ const DESIGNER = [
     id: 2,
     title: "designer photo2",
     imgURL:
-      "https://chalet-webapp.s3.amazonaws.com/designer-photos/IMG_4135.JPG",
+      "https://chalet-webapp.s3.amazonaws.com/designer-photos/IMG_1417.JPG",
   },
   {
     id: 3,
     title: "designer photo3",
     imgURL:
-      "https://chalet-webapp.s3.amazonaws.com/designer-photos/IMG_1148.JPG",
+      "https://chalet-webapp.s3.amazonaws.com/designer-photos/IMG_1131.JPG",
   },
 ];
 

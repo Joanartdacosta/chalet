d1309f79f18e43ed64883ca04bbfef07ed4bd292
joanartdacosta <joanartdacosta@gmail.com>
2023-10-30 11:24:03 +0100

    fix homepage - chalet - letter spacing, responsive

--- app/components/homepage/ChaletEntrance.js
+++ app/components/homepage/ChaletEntrance.js
@@ -1,18 +1,19 @@
 export default function ChaletBackground() {
   return (
-    <div className="background-chalet max-width-100 margin-auto background-cover background-norepeat height-60 background-size-100 ">
-      <h1 className="font-text font-size-6-25 text-center letter-space-2 padding-top-5 media-padding-top">
-        CHALET
+    <div className="background-chalet margin-auto background-cover height-60 background-media">
+      <h1 className="font-text font-size-5 letter-space-2 text-center padding-top-5 media-padding-top">
+        CHALE
+        <span className=" letter-space-0">T</span>
       </h1>
-      <hr className="border-black width-37-5 margin-auto" />
+      <hr className="border-black width-35 margin-auto" />
       <h1 className="font-subtitle font-size-11 font-bold text-center letter-space-02 padding-top-1-25">
-        DESIGN de INTERIORES
+        DESIGN de INTERIORE<span className="letter-space-0">S</span>
       </h1>
       <p className="font-subtitle font-size-11 font-bold text-center padding-top-1-25">
         by
       </p>
       <h1 className="font-subtitle font-size-11 font-italic text-center letter-space-02 padding-top-1-25 ">
-        Mariana Batista Ramadas
+        Mariana Batista Ramada<span className="letter-space-0">s</span>
       </h1>
     </div>
   );
